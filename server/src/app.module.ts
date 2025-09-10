a8e20bf8ef4c071ba4698d2444f65f5d8f82f62b
jules.corneille <jules.corneille@uha.fr>
2025-09-10 16:41:29 +0200

    STYLE-EVIX-delete-comment

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,10 +5,7 @@ import { HealthController } from './health/health.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    // Correction : utiliser un nom de DB différent du service
-    MongooseModule.forRoot('mongodb://localhost:27017/myapp-dev'),
-  ],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/myapp-dev')],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
