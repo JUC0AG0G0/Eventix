98940091422ace8515ecd6e8bf7306e77fe7ecb5
jules.corneille <jules.corneille@uha.fr>
2025-09-17 14:39:45 +0200

    Style-EVIX-ajout-config-preitier-eslint

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,34 +1,48 @@
 // @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
-  },
+	{
+		ignores: ["eslint.config.mjs", "dist", "node_modules"],
+	},
+	eslint.configs.recommended,
+	...tseslint.configs.recommendedTypeChecked,
+	eslintPluginPrettierRecommended,
+	{
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.jest,
+				...globals.browser, //pour le front
+			},
+			sourceType: "commonjs",
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+	{
+		rules: {
+			"@typescript-eslint/no-explicit-any": "off",
+			"@typescript-eslint/no-floating-promises": "warn",
+			"@typescript-eslint/no-unsafe-argument": "warn",
+
+			eqeqeq: ["error", "always"],
+			"no-var": "error",
+			"prefer-const": "warn",
+			"no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+
+			curly: ["error", "all"],
+			"default-case": "warn",
+			"no-fallthrough": "error",
+
+			quotes: "off",
+			semi: ["error", "always"],
+			"comma-dangle": ["error", "always-multiline"],
+		},
+	}
 );
